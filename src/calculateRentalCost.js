bfc65e8451c5a8509f32bb54ea71ee78a0fe96d9
lewis_mills <lewismills290@protonmail.com>
2025-04-28 09:27:27 +0100

    develop

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dailyRate = 40;
+  let totalCost = days * dailyRate;
+
+  if (days >= 7) {
+    totalCost -= 50; // $50 discount for 7 or more days
+  } else if (days >= 3) {
+    totalCost -= 20; // $20 discount for 3 or more days
+  }
+
+  return totalCost;
 }
+
 
 module.exports = calculateRentalCost;
